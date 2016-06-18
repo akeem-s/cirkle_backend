93c9ff657fc7064cc794041e45dc4a1f9fc5599d
TimBek2 <tim.beck46@gmail.com>
2016-06-18 17:33:49 -0400

    Add controllers for all three models and update routes file

---
+++ app/controllers/bids_controller.rb
@@ -0,0 +1,18 @@
+class BidsController < ApplicationController
+	#bids index also only available for admin to see macro data
+	def index
+	end
+
+###################################################################################
+
+	#show the result of a bid - conditional for win or lose based on RNG
+	def show
+	end
+	
+	#no bid#new route since that page is covered by the item - show page, which redirects to bid#create
+
+	#create route redirects to the show page which has the result
+	def create
+	end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+	#list of all users for admin only
+	def index
+	end
+
+###################################################################################
+
+	#landing page for the app - instructions and pull up barcode scanner button
+	def welcome
+	end
+
+	#user profile page - items bid, items won, etc
+	def show
+	end
+
+end
