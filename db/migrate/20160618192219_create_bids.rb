62263e449314bbe1e00f88a45bf71b1f1471a38d
TimBek2 <tim.beck46@gmail.com>
2016-06-18 21:28:50 -0400

    Add indexes to user and item references columns in bid migration

--- db/migrate/20160618192219_create_bids.rb
+++ db/migrate/20160618192219_create_bids.rb
@@ -1,8 +1,8 @@
 class CreateBids < ActiveRecord::Migration
   def change
     create_table :bids do |t|
-    	t.references :user, null: false
-    	t.references :item, null: false
+    	t.references :user, null: false, index: true
+    	t.references :item, null: false, index: true
     	t.boolean :winner, null: false
 
       t.timestamps null: false
